9b04242e81dfb5aa9141ee6611da737c023e57e3
mcc <46834427+Minccc@users.noreply.github.com>
2019-12-11 14:38:51 +0800

    1.0

--- table.h
+++ table.h
@@ -5,7 +5,24 @@
 #include<map>
 
 using namespace std;
+/*
+enum tokenType {	//token类型
+	tokenKeyWord,	//关键字
+	tokenIde,			//标识符
+	tokenDeli,			//界符
+	tokenConst,		//常量
+};
 
+struct  token
+{
+	
+	
+	
+
+	tokenType type;	//类型
+	int position;		//坐标
+};
+*/
 enum synblTval { //类型
 	tvalInt,		// 0 整型
 	tvalFloat,	// 1 浮点型
@@ -24,19 +41,27 @@ enum synblCat { //符号种类
 	catV,				// 4 变量
 	catVn,			// 5 换名形参
 	catVf,			// 6 赋值形参
-	catKeyWord	// 7 关键字
+	//catKeyWord	// 7 关键字
 };
 
-struct synbl   //符号表
-{
+enum delimiter {//界符
+
+};
+
+enum Keyword {	//
+
+};
+
+struct synbl{  //符号表
+
 	string name; //名字
 	synblTval type;//类型
-	int typePosition;	//类型坐标
+	int typePosition;	//类型坐标   
 	synblCat cat;//种类
 	int addr;		//地址
 };
 
-struct typel //类型表
+struct tapel //类型表
 {
 	synblTval tval;//类型
 	int position;	//坐标（0开始）
@@ -44,24 +69,22 @@ struct typel //类型表
 
 
 
-struct arryTval   //数组
+struct ainfl   //数组
 {
 	int low=0;				//数组下界
 	int up;					//数组上界
 
-	//synblTval arrtCtp;	//数组类型 指针
-
-	typel type;		//类型
+	tapel type;		//类型
 	int position;	//坐标
 	int arryClen;			//数组长度  字长
 };
 
-struct  rinflTval //结构体
+struct  rinfl //结构体
 {
 	string rinflId;		//结构的域名
 	int rinflOff;			//区距
 	//synblTval structType; //类型 指针
-	typel type;			//类型指针
+	tapel type;			//类型
 	int position;		//坐标
 };
 
@@ -72,7 +95,6 @@ struct pfinfl  //函数
 	int pfinflOff;		//区距
 	int pfinflFn;		//参数个数
 	int pfinflParam;	//参数表  指针
-	//auto pfinflEntry;	//入口地址
 	int entry;		//入口地址
 };
 
@@ -92,26 +114,32 @@ struct  vall   //活动记录
 };
 
 
-class table
+class table//符号表，在语义分析时将标识符的各信息填入词表
 {
 public:
 
 
 	table();
 
+
+
 	~table();
 	
-	void init();
+	void clear();//初始化各表
 
 
 private:
-	vector<synbl> synblTable;
 
 
+	vector<synbl> synblTable;	//符号表总表
+	vector<tapel> tapelTable;	//类型表
+	vector<ainfl> ainflTable;		//数组表
+	vector<rinfl> rinflTable;		//结构表
+	vector<pfinfl> pfinflTable;	//函数表
+	vector<consl> conslTable;	//常数表
+	vector<lenl> lenlTable;		//长度表
+	vector<vall> vallTable;		//活动记录表
 
 };
 
-table::~table()
-{
-}
 
