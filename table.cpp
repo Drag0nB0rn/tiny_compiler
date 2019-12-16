239648306cae17effac8955f1478d16b05f205b3
mcc <46834427+Minccc@users.noreply.github.com>
2019-12-16 20:32:48 +0800

    Add files via upload

--- table.cpp
+++ table.cpp
@@ -1,4 +1,60 @@
+#include "table.h"
+
+void table::clear()//各表初始化
+{
+	synblTable.clear();
+	tapelTable.clear();
+	ainflTable.clear();
+	rinflTable.clear();
+	pfinflTable.clear();
+	conslTable.clear();
+	lenlTable.clear();
+	vallTable.clear();
+}
+
+table::table()
+{
+
+	clear();
 
 
+}
 
-iint main()
+int table::getAddr(const int pos)
+{
+	return synblTable[pos].addr;
+}
+
+synblCat table::getCat(const int pos)
+{
+
+	return synblTable[pos].cat;
+}
+
+synblTval table::getTval(const int pos)
+{
+	return synblTable[pos].type;
+}
+
+int table::size()
+{
+	return synblTable.size();
+}
+
+bool table::isactive(const int pos)
+{
+	return synblTable[pos].active;
+}
+
+int table::getValue(const int pos)
+{
+	if (synblTable[pos].type == tvalInt) {//应返回值，暂时返回地址，后期修改
+		return synblTable[pos].addr;
+	}
+	return 0;
+}
+
+bool table::istmp(const int pos)
+{
+	return false;
+}
--- table.h
+++ table.h
@@ -5,6 +5,7 @@
 #include<map>
 
 using namespace std;
+
 /*
 enum tokenType {	//token类型
 	tokenKeyWord,	//关键字
@@ -23,6 +24,7 @@ struct  token
 	int position;		//坐标
 };
 */
+
 enum synblTval { //类型
 	tvalInt,		// 0 整型
 	tvalFloat,	// 1 浮点型
@@ -41,7 +43,6 @@ enum synblCat { //符号种类
 	catV,				// 4 变量
 	catVn,			// 5 换名形参
 	catVf,			// 6 赋值形参
-	//catKeyWord	// 7 关键字
 };
 
 enum delimiter {//界符
@@ -59,6 +60,7 @@ struct synbl{  //符号表
 	int typePosition;	//类型坐标   
 	synblCat cat;//种类
 	int addr;		//地址
+	bool active;	//活跃与否
 };
 
 struct tapel //类型表
@@ -66,8 +68,6 @@ struct tapel //类型表
 	synblTval tval;//类型
 	int position;	//坐标（0开始）
 };
-
-
 
 struct ainfl   //数组
 {
@@ -113,7 +113,6 @@ struct  vall   //活动记录
 	//
 };
 
-
 class table//符号表，在语义分析时将标识符的各信息填入词表
 {
 public:
@@ -121,13 +120,25 @@ public:
 
 	table();
 
+	int getAddr(const int pos);//获得标识符地址
 
+	synblCat getCat(const int pos);//获得标识符种类  是函数？ 常量？ 变量？
+
+	synblTval getTval(const int pos);//获得标识符类型   int，float，char，。。。
+
+	int size(); //返回synblTable符号总表的大小，即标识符个数
+
+	bool isactive(const int pos);//返回当前标识符活跃标志
+
+	int getValue(const int pos);//返回pos标识符的值，仅当pos为变量是有效，若为其他值，返回0
+
+	bool istmp(const int pos);
 
 	~table();
 	
 	void clear();//初始化各表
 
-
+	
 private:
 
 
@@ -143,3 +154,45 @@ private:
 };
 
 
+table idTable;	//标识符表，即符号表
+
+
+/*
+暂定：
+
+对于if语句
+
+1	( if , t1 ,  ,addr)	//如果t1为假，则跳转至目标地址addr 此例中为4,即else后的指令地址
+2	( =, 2  ,  ,a	)
+3	( else ,   ,  ,  addr   )//此处，在t1位true的情况下，应保存以上操作后结果。并跳出else后的操作 addr 为目标地址
+4	( =, 3  ,  ,  a)
+4	(  ,	,	,	)//此处已跳出if else语句；
+
+
+
+*/
+
+//暂时不写带函数的，即只有一张表，后期再添加多函数功能。
+
+struct fourelement
+{
+	string fix;//运算符 +-*/ > < = if，else fun call endcall
+	int pos1=-1, pos2=-1, pos3=-1;//三个目标操作数的地址,全部初始化为-1，-1代表空
+};
+
+
+vector<fourelement> four;			//四元式序列
+
+/*
+//前段
+void lex();		//词法分析，逐步解析输入流，每得到一个token就调用语法分析，若分析成功，则继续解析token，否则报错
+
+bool sym();	//语法分析，在一个token到来之后，执行语法分析，至当前符空，并在栈顶为语义动作时，调用语义分析，若成功，则返回true，否则返回false。
+
+bool syant();	//语义分析，在语法分析遇到语义动作后执行语义动作，并填符号表，最终给出四元式；
+
+//后端
+void better(); //在语义分析结束后，优化四元式,
+
+void compiler();	//将四元式翻译成汇编，并附带寄存器优化。
+*/
