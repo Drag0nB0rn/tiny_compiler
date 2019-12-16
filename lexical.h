2d4450d5589c3361718164c91ebc3bceeb14600f
Mrzxiao <45708682+Mrzxiao@users.noreply.github.com>
2019-12-16 21:10:43 +0800

    Update lexical.h

--- lexical.h
+++ lexical.h
@@ -1,3 +1,37 @@
-#include "table.h"
+#pragma once
+#include <SDKDDKVer.h> // 包括 SDKDDKVer.h 将定义可用的最高版本的 Windows 平台。
 #include "preProcess.h"
-#include "lexical.h"
+
+using namespace std;
+
+char CH;
+string strToken = "";
+
+class scannerToken
+{
+public:
+	void getToken();
+	scannerToken();
+	~scannerToken();
+private:
+	vector<vector<string>> tableScanner;
+};
+
+scannerToken::scannerToken();
+
+
+scannerToken::~scannerToken();
+
+
+void concat();
+
+bool isNum(char c);
+
+
+bool isAlpha(char c);
+
+bool underline(char  c);
+
+int reserve(string s);
+
+void retract(FILE *fp);
