dc32a3d42a8b452ea634b2de26a95fbda1bced5c
Mrzxiao <45708682+Mrzxiao@users.noreply.github.com>
2019-12-17 06:08:21 +0000

    Update lexical.h

--- lexical.h
+++ lexical.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SDKDDKVer.h> // 包括 SDKDDKVer.h 将定义可用的最高版本的 Windows 平台。
 #include "preProcess.h"
+#include "table.h"
 
 using namespace std;
 
@@ -9,80 +10,76 @@ string strToken = "";
 
 const string reserveword[13] = { "if","while","int","double","main","float","char","do","else","return","cout","cin","printf" };
 
+struct token
+{
+	string type;
+	string name;
+	int position;
+};
+
+struct identy
+{
+	string type;
+	string name;
+	int position;
+};
+
+struct constant
+{
+	string type;
+	string name;
+	int position;
+};
+
+struct symbolString
+{
+	string type;
+	string name;
+	int position;
+};
+
+struct key
+{
+	string type;
+	string name;
+	int position;
+};
+
+struct del
+{
+	string type;
+	string name;
+	int position;
+};
+
 class scannerToken
 {
 public:
-	vector<vector<string>> getToken();
-	vector<string> getConslToken();
-	vector<string> getIdToken();
-	vector<string> getStringToken();
+	vector<token> getToken();
+	vector<constant> getConslToken();
+	vector<identy> getIdToken();
+	vector<symbolString> getStringToken();
+	vector<del>getDelimiterToken();
+	vector<key>getKeywordToken();
 
-	void setToken(vector<vector<string>> table);
-	void setConslToken(vector<string> consl);
-	void setIdToken(vector<string> id);
-	void setStringToken(vector<string> string);
 
-	scannerToken();
-	~scannerToken();
+	void setToken(string T,string S,int pos);
+	void setConslToken(string T, string S, int pos);
+	void setIdToken(string T, string S, int pos);
+	void setStringToken(string T, string S, int pos);
+	void setDelimiterToken(string T, string S, int pos);
+	void setKeywordToken(string T, string S, int pos);
+
 private:
-	vector<vector<string>> tableScanner;
-	vector<string> conslToken;
-	vector<string> idToken;
-	vector<string> stringToken;
+	vector<token> tableScanner;
+	vector<constant> conslToken;
+	vector<identy> idToken;
+	vector<symbolString> stringToken;
+	vector<del> delimiterToken;
+	vector<key> keywordToken;
 };
 
-scannerToken::scannerToken()
-{
-	for (int i = 0; i < 25; i++)
-	{
-		tableScanner[i].resize(2);
-	}
-}
 
-scannerToken::~scannerToken()
-{
-	cout << "token类已被析构" << endl;
-}
-
-vector<string> scannerToken::getConslToken()
-{
-	return conslToken;
-}
-
-vector<string> scannerToken::getIdToken()
-{
-
-}
-
-vector<string> scannerToken::getStringToken()
-{
-	return stringToken;
-}
-
-vector<vector<string>> scannerToken::getToken()
-{
-	return tableScanner;
-}
-
-void scannerToken::setToken(vector<vector<string>> table)
-{
-	tableScanner = table;
-}
-
-void scannerToken::setConslToken(vector<string> consl)
-{
-	conslToken = consl;
-}
-
-void scannerToken::setIdToken(vector<string> id)
-{
-	idToken = id;
-}
-
-void scannerToken::setStringToken(vector<string> string)
-{
-	stringToken = string;
-}
 
 void concat();
 
