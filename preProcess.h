796d09895690356a0c3e733a4736ec4073c53464
Mrzxiao <45708682+Mrzxiao@users.noreply.github.com>
2019-12-17 14:12:14 +0800

    preProcess

---
+++ preProcess.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+void preProcess();
+
+void init();
+
+void pretxt();
